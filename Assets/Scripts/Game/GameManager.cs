25e1bab0d875bb8eda98e92f7e792e1de8cbfcda
Щегольков Никита <Mousecach@gmail.com>
2016-04-25 19:29:42 +0300

    Добавление новых моделей и настройка врагов

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	int EnemyCounter = 0;
+	Transform TR;
+	Transform SpawnPoint;
+
+	public GameObject Enemy;
+
+	void Start()
+	{
+		Invoke ("SpawnEnemy", 1);
+		TR = transform;
+
+		SpawnPoint = TR.GetChild (0).transform;
+	}
+
+	void SpawnEnemy()
+	{
+		EnemyCounter++;
+
+		TR.Rotate (Random.Range (0, 360), Random.Range (0, 360), Random.Range (0, 360));
+
+		Instantiate (Enemy, SpawnPoint.position, SpawnPoint.rotation);
+
+		Invoke ("SpawnEnemy", Random.Range (1f, 3f));
+	}
+}
